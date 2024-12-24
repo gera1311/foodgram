03de52443accfc1ca038442ee73a405623e6ffa1
gera1311 <gera.python@yandex.ru>
2024-12-24 22:55:28 +0300

    For deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['foodgram.shop', '127.0.0.1', 'localhost']
 
 
 # Application definition
